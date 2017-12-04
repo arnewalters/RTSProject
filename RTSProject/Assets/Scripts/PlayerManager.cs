01d9a61c59e077df58bba424a84c8c07cb3d415c
ARNEPC\Arne <Arne.Walters@gmx.de>
2017-12-04 19:29:01 +0100

    Added playermanager

---
+++ RTSProject/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+    public static PlayerManager instance;
+
+    public string playerName;
+
+    private string[] firstnames = { "John", "Maria", "Dirk", "Cash", "Boy", "Dude"};
+    private string[] lastnames = {"Dill", "Rap", "Nap", "Noob", "Rude", "Sup"};
+
+    private void Awake()
+    {
+        #region Validate PlayerManager instance
+        if (instance == null)
+        {
+            instance = this;
+            playerName = GenerateRandomName();
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(transform.gameObject);
+        #endregion 
+    }
+
+    private string GenerateRandomName()
+    {
+        return firstnames[Random.Range(0, firstnames.Length)] + " " + lastnames[Random.Range(0, lastnames.Length)];
+    }
+}
