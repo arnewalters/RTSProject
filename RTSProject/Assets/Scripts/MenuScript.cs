2e838f04546f7d50bfec7031ac5e13c599c468b7
arnewalters <34208802+arnewalters@users.noreply.github.com>
2017-12-04 20:38:38 +0100

    added functionality of Menu Buttons

---
+++ RTSProject/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+    public GameObject playButton;
+    public GameObject settingsButton;
+    public GameObject mapEditorButton;
+    public GameObject quitButton;
+
+    void Start()
+    {
+        playButton = GameObject.FindGameObjectWithTag("playButton");
+        settingsButton = GameObject.FindGameObjectWithTag("settingsButton");
+        mapEditorButton = GameObject.FindGameObjectWithTag("mapEditorButton");
+        quitButton = GameObject.FindGameObjectWithTag("quitButton");
+    }
+
+    public void PlayButtonClicked()
+    {
+        SceneManager.LoadScene("Prototyping Scene");
+    }
+
+    public void SettingsButtonClicked()
+    {
+        //TODO Implement settings: Audio, Graphics, Miscelaneous, Hotkeys
+    }
+
+    public void MapEditorButtonClicked()
+    {
+        SceneManager.LoadScene("Map Editor Scene");
+    }
+
+    public void QuitButtonClicked()
+    {
+        Application.Quit();
+    }
+}
